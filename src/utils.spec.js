dd0658dd0d0928eaf032a7a8e25cc0fb1f7eaea2
Charles Davison <charlie@powmedia.co.uk>
2021-01-25 16:33:23 +0100

    Add tests

--- src/utils.spec.js
+++ src/utils.spec.js
@@ -7,8 +7,24 @@ import { replaceVars } from './utils';
 
 describe('utils', function() {
   describe('#replaceVars()', function() {
-    it('replaces {{mustache}} style vars', function() {
+    it('returns if not passed a string', function () {
+      same(replaceVars(null), null);
+      same(replaceVars(undefined), undefined);
+      same(replaceVars(1), 1);
+      test.deepEqual(replaceVars({ foo: 'bar' }), { foo: 'bar' });
+    });
+
+    it('replaces {{mustache}} style vars', function () {
       same(replaceVars('Hello {{name}}!', { name: 'Bob' }), 'Hello Bob!');
+    });
+
+    it('removes placeholders without a valuee', function () {
+      const result = replaceVars('Hello {{firstName}} {{lastName}}!', {
+        firstName: undefined,
+        lastName: 'Smith',
+      });
+
+      same(result, 'Hello  Smith!');
     });
   });
 });
